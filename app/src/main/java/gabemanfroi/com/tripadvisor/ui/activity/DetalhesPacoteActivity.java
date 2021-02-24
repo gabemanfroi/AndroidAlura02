339a7275ffc12748c661c9a0f240005adf397176
gabemanfroi <gabsmanfroi@gmail.com>
2021-02-24 17:09:39 -0300

    implementações iniciais da 2a parte do projeto

---
+++ app/src/main/java/gabemanfroi/com/tripadvisor/ui/activity/DetalhesPacoteActivity.java
@@ -0,0 +1,16 @@
+package gabemanfroi.com.tripadvisor.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import gabemanfroi.com.tripadvisor.R;
+
+public class DetalhesPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_pacote);
+    }
+}
